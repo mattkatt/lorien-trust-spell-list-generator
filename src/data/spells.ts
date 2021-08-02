ad14056806896bba5110d401b092900b16790c24
Matt Evans <m4ttk4tt@hotmail.co.uk>
2021-08-02 12:03:10 +0100

    Update spells.ts
    Added copyright notice to spells

--- src/data/spells.ts
+++ src/data/spells.ts
@@ -1,3 +1,9 @@
+/**
+ * All content in this file is copyright 2021 Lorien Trust - no reproduction or
+ * alteration of this data is permissable without permission from Lorien Trust
+ * or its parent company, Merlinroute.
+ */
+
 import { Characteristic, Class, Duration, ISpell, List, Range } from "../interface/spells"
 import {
     controlGroup,
